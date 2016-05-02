f07a9024304bbe2418b502b98009036f784c4f45
pepe-roni <ahde2k@gmail.com>
2016-05-01 19:02:29 -0700

    init:

---
+++ ESC_and_Electronic_Steering.ino
@@ -0,0 +1,74 @@
+#include <Servo.h>
+#include <LiquidCrystal.h>
+#include <LCDKeypad.h>
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+Servo steering, ESC;
+
+int turn = 90;
+int throttle = 95;
+boolean shift;
+
+void setup()
+{
+//shows ADM boot status
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("ADM Initializing");
+  delay(1000);
+
+//reset/init of steering 
+  steering.attach(1);
+  delay(100);
+  steering.wraaaaaaaaaite(180);
+  delay(200);
+  steering.write(0);
+  delay(200);
+  steering.write(90);
+  delay(200);
+  
+//reset/init of ESC
+  ESC.attach(0);
+  delay(200);
+  ESC.write(105);
+  delay(200);
+  ESC.write(90);
+  delay(200);
+
+//ready message for ADM
+  lcd.setCursor(0, 0);
+  lcd.print("   ADM Ready!   ");
+  delay(500);
+}
+
+void loop()
+{
+  lcd.setCursor(0,1);
+  lcd.print("SPEED: ");
+  lcd.setCursor(7,1);
+  lcd.print(throttle);
+  //stop = 95
+  ESC.write(throttle);
+  delay(250);
+  esc();
+  if (shift)
+  {
+    throttle -= 0.5;
+  }
+  else
+  {
+    throttle++;
+  }
+}
+
+void esc()
+{
+  if (throttle <= 70)
+  {
+    shift = false;
+  }
+  else if (throttle >= 95)
+    shift = true;
+}
+
+
